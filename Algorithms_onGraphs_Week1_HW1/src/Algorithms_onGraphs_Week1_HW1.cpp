c41840abe4b02c0d041e4c173cb2658697869116
danielramirez <danielramirez@192.168.0.3>
2016-08-31 19:55:09 -0500

    Implementing solution to check if there is a path between two vertices.

--- Algorithms_onGraphs_Week1_HW1/src/Algorithms_onGraphs_Week1_HW1.cpp
+++ Algorithms_onGraphs_Week1_HW1/src/Algorithms_onGraphs_Week1_HW1.cpp
@@ -3,23 +3,44 @@
 // Author      : Daniel Ramirez
 // Version     :
 // Copyright   : HW1_Week1
-// Description : Graph Representation in Programming Assignments in C++, Ansi-style
+// Description : Finding an exit from a Maze
 //============================================================================
 
 #include <iostream>
 #include <vector>
 
+#define constraints_n_m(n,m) ((n>2 && n<=10e3) && (m>1 && m<=10e3) )
+#define constraints_u_v(x,y) (x>=1 && x!=y)
+
 using std::vector;
 using std::pair;
+using std::cout;
 
+//prototypes
+void explore(vector<vector<int> > &adj, vector<bool> &adj_v, int x);
+int reach(vector<vector<int> > &adj, int x, int y);
+
+//Global variables
+size_t n, m;
+
+void explore(vector<vector<int> > &adj, vector<bool> &adj_v, int x){
+   vector<int>::iterator it;
+   adj_v[x]=true;
+   for(it=adj[x].begin();it!=adj[x].end();it++){
+	   if(adj_v[*it]==false)
+	       explore(adj,adj_v,*it);
+   }
+}
 
 int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+	vector<bool> adj_v(n,false);
+	explore(adj,adj_v,x);
+    if(adj_v[y]==true)
+    	return 1;
+    return 0;
 }
 
 int main() {
-  size_t n, m;
   std::cin >> n >> m;
   vector<vector<int> > adj(n, vector<int>());
   for (size_t i = 0; i < m; i++) {
@@ -30,6 +51,6 @@ int main() {
   }
   int x, y;
   std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+  std::cout << reach(adj,x - 1, y - 1);
 }
 
